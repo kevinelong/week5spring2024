ffd5e18609d302bc688ad359a196405ad9e4bc28
kevinelong <kevinelong@gmail.com>
2024-05-07 11:48:21 -0700

    for of

---
+++ demos/05_for_of.js
@@ -0,0 +1,20 @@
+
+const fruitList = ["apple", "orange", "pear"]; //array of strings
+
+for(s of fruitList){
+    console.log(s);
+}
+
+//////////////////////////
+
+const peopleList = [ ///array of objects
+    {name:"Kevin", amount: 123},
+    {name:"Nina", amount: 243},
+    {name:"Dandy", amount: 543},
+];
+
+for(person of peopleList){
+    console.log(person.amount)
+}
+//////////////////////
+
